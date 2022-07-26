03bacc86d449da9bba8d7d07a761320530094c02
Niku Grönberg <gronbergniku@gmail.com>
2022-07-26 21:27:06 +0300

    move mongo connection string to env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING || ''),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
